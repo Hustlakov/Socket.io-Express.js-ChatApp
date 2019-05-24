ebf41316e8f403d84f49639e821f6f4760a1137a
Hustlakov <extraspirit@list.ru>
2019-05-24 04:16:29 +0300

    send and display global message to everyone in default chatroom

--- app.js
+++ app.js
@@ -7,28 +7,29 @@ const io = socketIo(server);
 
 server.listen(80); //port 80 is the default port = http://localhost
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // __dirname here = D:\testing-socketio
+
 app.get("/", (req, res) => {
-  // __dirname here = D:\testing-socketio
   res.sendFile("/index.html");
 });
 
 app.get("/room1/:id", (req, res) => {});
 
 io.on("connection", socket => {
-  console.log("Socket connected with id: " + socket.id);
-  socket.emit("news", { hello: "world" });
-  socket.on("my other event", data => {
-    console.log(data);
-  });
-  socket.on("questionToServer", data => {
+  // console.log("Socket connected with id: " + socket.id);
+  // socket.emit("news", { hello: "world" });
+  // socket.on("my other event", data => {
+  //   console.log(data);
+  // });
+  socket.on("submitMessage", data => {
     //Send to everyone except self
-    socket.broadcast.emit("answerToClient", {
-      chat: "Hey client, I am server and I can see you"
-    });
+    // socket.broadcast.emit("answerToClient", {
+    //   text
+    // });
+
     //Send to everyone including self
-    io.emit("answerToEveryone", {
-      chat2: "This Message is for everyone including myself"
+    io.emit("sendGlobalMessage", {
+      text: data.text
     });
   });
 });
--- client.js
+++ client.js
@@ -1,37 +1,24 @@
 const socket = io(); //io.connect("http://localhost") for older version
 
-// let inputMsg = () => {
-//   console.log("clicked");
-//   let msg = document.querySelector("#myInput");
-//   socket.emit("questionToServer", { data: msg.value });
-// };
-
 let inputMsg = event => {
-  console.log("keypress");
   const text = event.target.value;
   if (event.keyCode === 13 && text) {
-    socket.emit("questionToServer", { data: text });
+    socket.emit("submitMessage", { text }); //ES6 standard about objects key-value being same name we can write "text" instead of "text": "text"
     event.target.value = "";
-    console.log("keycode works");
   }
 };
 
-socket.on("news", function(data) {
-  console.log(`Socket id: ${socket.id}`, "\n", data);
-  socket.emit("my other event", { my: "bata" });
-});
-
-socket.on("answerToEveryone", data => {
+socket.on("SendGlobalMessage", data => {
   let li = document.createElement("li");
   let ul = document.querySelector("#myUl");
+  let countLiInUl = ul.childElementCount;
 
-  li.classList.add("list-group-item");
-  ul.appendChild(li).innerHTML = data.chat2;
-  let countLiInUl = document.querySelectorAll("#myUl li").length;
+  if (countLiInUl >= 9) ul.firstChild.remove();
+  ul.firstChild.remove();
 
-  if (countLiInUl >= 9) ul.removeChild(ul.firstChild);
-  // let output = (document.querySelector("#myOutput").innerHTML = data.chat2);
-});
-socket.on("answerToClient", dataMsg => {
-  console.log(dataMsg.chat);
+  li.classList.add("list-group-item", "text-center");
+  newLi = ul.appendChild(li);
+  ul.lastChild.innerHTML = data.text;
+
+  console.log(`Count Li elements in Ul = ${countLiInUl}`);
 });
